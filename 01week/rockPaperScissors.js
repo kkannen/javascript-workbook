3bc96e2d347ee111ca3d303a5b4110c25afc588e
kkannen <kristamkannen@gmail.com>
2018-01-30 13:59:10 -0600

    trim

--- 01week/rockPaperScissors.js
+++ 01week/rockPaperScissors.js
@@ -9,8 +9,8 @@ const rl = readline.createInterface({
 
 
 function rockPaperScissors(hand1, hand2) {
-  hand1 = hand1.toLowerCase();
-  hand2 = hand2.toLowerCase();
+  hand1 = hand1.toLowerCase().trim();
+  hand2 = hand2.toLowerCase().trim();
   if (hand1 === hand2) {
     return "It's a tie!"
   } else if ((hand1 === "rock" && hand2 === "scissors") || (hand1 === "paper" && hand2 === "rock") || (hand1 === "scissors" && hand2 === "paper")) {
