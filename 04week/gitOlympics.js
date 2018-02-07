585870eff09c619faa384e1daf8233f94aa9d03c
kkannen <kristamkannen@gmail.com>
2018-02-06 19:16:10 -0600

    second commit, hopefully clean?

--- 04week/gitOlympics.js
+++ 04week/gitOlympics.js
@@ -1,5 +1,18 @@
 'use strict';
-
+  const Olympians = ["name1", "name2", "name3", "name4", "name5"]
 const printListOfOlympians = (arr) => {
-  const arr = ["name1", "name2", "name3", "name4"]
+  arr.forEach((olympian)=>{
+    console.log(olympian)
+  })
 }
+
+
+// var array1 = ['a', 'b', 'c'];
+//
+// array1.forEach(function(element) {
+//   console.log(element);
+// });
+//
+// // expected output: "a"
+// // expected output: "b"
+// // expected output: "c"
