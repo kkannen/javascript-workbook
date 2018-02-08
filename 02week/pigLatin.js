5867420b88629ae12870fbfba3bae8be08be2100
kkannen <kristamkannen@gmail.com>
2018-02-08 15:25:32 -0600

    this code is working I think

--- 02week/pigLatin.js
+++ 02week/pigLatin.js
@@ -8,12 +8,30 @@ const rl = readline.createInterface({
 });
 
 
-function pigLatin(word) {
 
-  // Your code here
 
-}
-
+const pigLatin = (word) => {
+  const vowel = ['a', 'e', 'i', 'o', 'u', 'y'];
+  const formattedWord = word.toLowerCase().trim(); //lowercase word, nospaces
+  const letters = formattedWord.split(''); //makes array out of letters in word
+  for (i = 0; i < letters.length; i++){
+    for (j = 0; j < vowel.length; j++){
+      const indexOfVowel = letters.indexOf(vowel[j])
+      let firstLetters = ''
+      if(letters[0] === vowel[5]){
+        return word.slice(1) + 'yay'
+      } else if (letters[0] === vowel[j]) {
+        return word + 'yay'
+      } else if (letters[i] === vowel[j] && letters.indexOf(vowel[j] > 0)){
+        firstLetters = letters.splice(0, indexOfVowel).join('')
+        const lettersToEnd = letters.push(firstLetters + 'ay')
+        const translatedWord = letters.join('')
+        return translatedWord
+      }//if statement
+    }//vowels loop
+  }//letters loop
+}//pigLatin
+console.log(pigLatin('car'))
 
 function getPrompt() {
   rl.question('word ', (answer) => {
