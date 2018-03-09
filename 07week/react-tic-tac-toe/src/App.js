6027c8592bd3d5db65e1b5db47b97666cb48ef1b
kkannen <kristamkannen@gmail.com>
2018-03-09 08:08:33 -0600

    lol

--- 07week/react-tic-tac-toe/src/App.js
+++ 07week/react-tic-tac-toe/src/App.js
@@ -1,32 +1,33 @@
 import React, { Component } from 'react';
 import './App.css';
 
-class TicTacToe extends Component {
+class App extends Component {
   constructor(props) {
     super(props);
+      this.state = {
+        playerTurn: 'X',
+        moves: 0,
+        board: [
+          [null, null, null],
+          [null, null, null],
+          [null, null, null]],
+    }
+  }
+
+  handlePlayerMove = () => {
+    console.log('pooooo')
   }
 
   render() {
     return (
       <div>
-        <div className="row">
-          <div data-cell="0"></div>
-          <div data-cell="1"></div>
-          <div data-cell="2"></div>
-        </div>
-        <div className="row">
-          <div data-cell="3"></div>
-          <div data-cell="4"></div>
-          <div data-cell="5"></div>
-        </div>
-        <div className="row">
-          <div data-cell="6"></div>
-          <div data-cell="7"></div>
-          <div data-cell="8"></div>
-        </div>
+        {this.state.board.map((row) => {
+          return <div className = "square">poo</div>
+        })}
+
       </div>
     );
   }
 }
 
-export default TicTacToe;
+export default App;
